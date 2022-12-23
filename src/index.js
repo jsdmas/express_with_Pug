6e8c4d47884e5d35a68fbfa6ea7101b84c8a7ec1
jsdmas <sdmas5730@gmail.com>
2022-12-23 17:14:15 +0900

    router, controllers

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// morgan은 node.js용 request logger middleware 이다.
+import express from "express";
+import morgan from "morgan";
+import globalRouter from './routers/globalRouter';
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
+
+const app = express();
+const PORT = 4000;
+const logger = morgan("dev");
+app.use(logger);
+
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+const handleServer = (req, res) => console.log(`server listen http://localhost:${PORT}! ✅`);
+app.listen(PORT, handleServer);
